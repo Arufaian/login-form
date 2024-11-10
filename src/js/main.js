e0a0f0bb48ce196f91bd2f417f95181e881f5702
Alfian <alfianilyasya13@gmail.com>
2024-11-10 07:51:49 +0700

    adding responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
+
+// let width = screen.width;
+
+// if (width < 768) {
+// }
+
+const leftBox = document.querySelector("#left-box");
+
+window.addEventListener("resize", () => {
+  const screenWidth = window.innerWidth;
+  if (screenWidth < 768) {
+    leftBox.classList.add("d-none");
+  } else {
+    leftBox.classList.remove("d-none");
+  }
+});
